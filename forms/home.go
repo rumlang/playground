cf306b6035791656219451c0b1b6c91994c6663e
trumae <trumae@gmail.com>
2018-02-08 16:11:09 -0200

    first commit

---
+++ forms/home.go
@@ -0,0 +1,123 @@
+package forms
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gorilla/websocket"
+	"github.com/rumlang/rum/parser"
+	"github.com/rumlang/rum/runtime"
+	"github.com/trumae/valente"
+	"github.com/trumae/valente/action"
+	"github.com/trumae/valente/elements"
+)
+
+func rumParse(s string) (*parser.Value, error) {
+	v, err := parser.Parse(parser.NewSource(s))
+	if err != nil {
+		return nil, err
+	}
+	return &v, nil
+}
+
+func rumEval(s string, c *runtime.Context) (*parser.Value, error) {
+	ival, err := rumParse(s)
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := c.TryEval(*ival)
+	if err != nil {
+		return nil, err
+	}
+	return &val, nil
+}
+
+//RumReplForm struct
+type RumReplForm struct {
+	valente.FormImpl
+}
+
+//Render the initial html form to FormRumrepl
+func (form RumReplForm) Render(ws *websocket.Conn, app *valente.App, params []string) error {
+	content := elements.Panel{}
+	content.AddClass("container")
+
+	content.AddElement(elements.Heading3{Text: "RumREPL"})
+
+	rowbtn := elements.Panel{}
+	rowbtn.AddClass("row")
+
+	btnRun := elements.Button{Text: "Run", PostBack: []string{"run"}}
+	btnRun.AddClass("btn btn-primary col-4")
+
+	btnClean := elements.Button{Text: "Clean", PostBack: []string{"clean"}}
+	btnClean.AddClass("btn btn-secondary col-4 offset-4")
+
+	rowbtn.AddElement(btnRun)
+	rowbtn.AddElement(btnClean)
+	content.AddElement(rowbtn)
+
+	codeta := elements.TextArea{}
+	codeta.ID = "input"
+	codeta.AddClass("form-control")
+	codeta.SetData("rows", "20")
+	codeta.SetData("cols", "80")
+	codeta.SetStyle("height", "300px")
+	codeta.SetStyle("width", "100%")
+	codeta.SetStyle("margin-top", "10px")
+	content.AddElement(elements.Heading4{Text: "Input"})
+	content.AddElement(codeta)
+
+	output := elements.Panel{}
+	output.ID = "output"
+	output.SetStyle("height", "300px")
+	output.SetStyle("width", "100%")
+	output.SetStyle("background-color", "#EEEEEE")
+	output.SetStyle("overflow", "auto")
+	content.AddElement(elements.Heading4{Text: "Output"})
+	content.AddElement(output)
+
+	action.HTML(ws, "content", content.String())
+	return nil
+}
+
+func runRumRepl(ws *websocket.Conn, app *valente.App, params []string) {
+	context, ok := app.Data["runContext"]
+	if !ok {
+		context = runtime.NewContext(nil)
+		app.Data["runContext"] = context
+	}
+	code, err := action.Get(ws, "input")
+	if err != nil {
+		log.Println(err)
+	}
+
+	if len(code) > 0 {
+		action.Append(ws, "output", "<p> >> <b>"+code+"</b><p>")
+
+		v, err := rumEval(code, context.(*runtime.Context))
+		if err != nil {
+			action.Append(ws, "output", fmt.Sprint("<p>Err: ", err, "</p><br/>"))
+		}
+
+		action.Append(ws, "output", "<p>-> "+fmt.Sprintf("%v</p>", (*v).Value()))
+
+		action.Exec(ws, "$('#output').scrollTop($('#output')[0].scrollHeight)")
+	}
+}
+
+func cleanRumRepl(ws *websocket.Conn, app *valente.App, params []string) {
+	action.HTML(ws, "output", "")
+	action.Set(ws, "input", "")
+}
+
+//Initialize inits the Rumrepl Form
+func (form RumReplForm) Initialize(ws *websocket.Conn) valente.Form {
+	log.Println("RumreplForm Initialize")
+
+	f := form.AddEventHandler("run", runRumRepl)
+	f = f.AddEventHandler("clean", cleanRumRepl)
+
+	return f
+}
---
+++ main.go
@@ -0,0 +1,149 @@
+//go:generate go-bindata-assetfs public/...
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/satori/go.uuid"
+	"github.com/trumae/rumplay/forms"
+	"github.com/trumae/valente"
+	"github.com/trumae/valente/status"
+
+	"github.com/gorilla/websocket"
+)
+
+//App is a Web Application representation
+type App struct {
+	valente.App
+}
+
+const timeout = 300
+const gctime = 1
+
+var (
+	sessions map[string]*App
+	mutex    sync.Mutex
+
+	upgrader = websocket.Upgrader{}
+)
+
+//addSession include a new app on sessions
+func addSession(key string, app *App) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	sessions[key] = app
+}
+
+//getSession return the app by key
+func getSession(key string) *App {
+	return sessions[key]
+}
+
+func gcStepSession() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	now := time.Now().Unix()
+	for key, app := range sessions {
+		if now-app.LastAccess.Unix() > timeout {
+			log.Println("Collecting", key)
+			delete(sessions, key)
+		}
+	}
+}
+
+//Initialize inits the App
+func (app *App) Initialize() {
+	log.Println("App Initialize")
+
+	app.AddForm("home", forms.RumReplForm{})
+
+	app.GoTo("home", nil)
+}
+
+func main() {
+	port := "8000"
+	flag.StringVar(&port, "port", "8000", "http port")
+
+	flag.Parse()
+
+	log.Println("Init sessions")
+	sessions = make(map[string]*App)
+	mutex = sync.Mutex{}
+
+	go func() {
+		for {
+			time.Sleep(gctime * time.Second)
+			gcStepSession()
+		}
+	}()
+
+	fs := http.FileServer(assetFS())
+	http.Handle("/", fs)
+
+	http.HandleFunc("/status", status.ValenteStatusHandler)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer ws.Close()
+
+		err = ws.WriteMessage(websocket.TextMessage, []byte("__GETSESSION__"))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		_, bid, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		idSession := string(bid)
+
+		var app *App
+		app = getSession(idSession)
+		if app == nil {
+			su1, err := uuid.NewV4()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			u1 := su1.String()
+			log.Println("New session", u1)
+			app = &App{}
+			app.LastAccess = time.Now()
+			addSession(u1, app)
+			err = ws.WriteMessage(websocket.TextMessage, []byte(u1))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			app.WebSocket(ws)
+			app.Initialize()
+		} else {
+			app.LastAccess = time.Now()
+			log.Println("Reusing session", idSession)
+			err := ws.WriteMessage(websocket.TextMessage, []byte(idSession))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			app.WebSocket(ws)
+		}
+		app.Run()
+	})
+
+	log.Println("Server running ...")
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
